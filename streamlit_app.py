00f7b5dcb90e2033ac2375a14c02dc87edeeab28
Владимир Доценко <89936103+t74sten@users.noreply.github.com>
2023-07-01 17:14:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ with col12:
     inflight_service = on_board_service
     cleanliness = st.slider("Чистота салона", min_value=1, max_value=5, step=1, value=1)
 
-if st.sidebar.button('Сбросить'):
+if st.button('Сбросить'):
     # Clear all widget fields
     st.sidebar.empty()
     st.empty()
