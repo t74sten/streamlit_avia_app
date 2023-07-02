0fab93d960a7c7103ed06ae16af69c400fa3e187
Владимир Доценко <89936103+t74sten@users.noreply.github.com>
2023-07-02 12:30:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,7 +125,7 @@ with col1:
 
 with col3:
     st.markdown('##### Узнать мнение Ваших клиентов')
-    st.text('Можете кинуть скачанный шаблон в поле ниже, там предзаполнена одна строка для проверки, спасибо!)')
+    st.text('Можете кинуть скачанный шаблон в поле ниже,\n там предзаполнена одна строка для проверки,\n спасибо!)')
     st.download_button(label="Скачать шаблон в CSV", data=convert_df(pd.read_csv('data/shablon_s.csv')),
                        file_name='shablon_s.csv', mime='text/csv',)
 
